91204806718aae9581e91d850847723a852bf880
EC2 Default User <ec2-user@ip-172-31-7-119.ap-northeast-1.compute.internal>
2025-01-19 04:50:21 +0000

    [Finish]users controllerを作っただけ,書いたところはuser.rb内のみ

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  def new
+    @book =Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id =current.user_id
+    @book.save
+    redirect_to books_path
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+  
+  private
+
+  def book_params
+    params.requeire(:book).permit(:title,:body,:image)
+  end
+
+end
--- db/migrate/20250118120144_devise_create_users.rb
+++ db/migrate/20250118120144_devise_create_users.rb
@@ -34,9 +34,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
 
       ## 名前を保存するカラム
       t.string :name
-
-
-
+      t.text :introduce
+      
       t.timestamps null: false
     end
 
