32b74c0fb4a13f1fad8e95134636c9c451337cde
EC2 Default User <ec2-user@ip-172-31-7-119.ap-northeast-1.compute.internal>
2025-01-21 00:37:14 +0000

    [Finish]destroy,but leftpage,(index,and,show)pagetemplete yet

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about", as:'about'
-  resources :books, only:[:new,:index,:show,:create]
+  resources :books
   resources :users
 end
